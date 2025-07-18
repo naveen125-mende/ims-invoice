4a4d7ea6c77a242f5e872e53013df18ab1619bfe
Naveen_mende <mendenaveenyadav125@gmail.com>
2025-07-18 12:19:09 +0530

    pdf changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import { InvoiceDetails } from './constants';
+import { async } from 'rxjs';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +12,34 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ims-invoice';
+  invoiceDetails = InvoiceDetails;
+  downloadPdf() {
+    const downloadData = {
+      invoiceData: this.invoiceDetails,
+      billedBy: {
+        name: this.invoiceDetails.store.storeName,
+        contactNumber: this.invoiceDetails.store.contactNumber,
+        email: this.invoiceDetails.store.email,
+      },
+      billedTo: {
+        name: this.invoiceDetails.customer.customerName,
+        contactNumber: this.invoiceDetails.customer.contactNumber,
+        email: this.invoiceDetails.customer.email,
+      },
+      invoiceItems: this.invoiceDetails.saleItems,
+      invoiceType: 'Sale Invoice',
+      payments: this.invoiceDetails.salePayments,
+    };
+    
+  }
+  async generateJsPdf(downloadData: any): Promise<void> {
+    const imageUrl = '../../public/assets/images/devlats_logo.png';
+    const doc = new jsPDF();
+    const pageWidth = doc.internal.pageSize.width;
+    const pageHeight = doc.internal.pageSize.height;
+    doc.setFont('helvetica', 'normal');
+    doc.setFontSize(12);
+    doc.setTextColor(32, 34, 36);
+    doc.setTextColor(0, 0, 0);
+  }
 }
