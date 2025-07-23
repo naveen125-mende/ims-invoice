b324f8a6a94ac9b4e0b8461625050b68cacf9d7e
saimaggidi <maggidisai4@gmail.com>
2025-07-23 10:11:32 +0530

    saleinvoice-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -249,34 +249,68 @@ export class AppComponent {
     });
     doc.setFont('helvetica', 'normal');
     doc.setFontSize(10);
-    doc.text('Tax Amount  (in words):', 6, 196)
-    doc.text('INR Five thousand nine hundred Only', 6, 202)
-    doc.text('Payment Type: Cash', 6, 208)
-    doc.text('Total Weight: 0.2kg', 6, 214)
     doc.setFont('helvetica', 'bold');
-    doc.text("Company's Bank Details", 6, 224)
+    doc.text('Tax Amount  (in words):', 59, 197)
     doc.setFont('helvetica', 'normal');
-    doc.text('Bank Name: Axis Bank', 6, 230)
-    doc.text('A/c No : 00000000', 6, 236)
-    doc.text('Branch & IFS CODE : 357657', 6, 242)
+    doc.text('INR Five thousand nine hundred Only', 59, 203)
+    doc.text('Payment Type: Cash', 59, 209)
+    doc.text('Total Weight: 0.2kg', 59, 215)
+    doc.setFont('helvetica', 'bold');
+    doc.text("Company's Bank Details", 6, 197)
+    doc.setFont('helvetica', 'normal');
+    doc.text('Bank Name: Axis Bank', 6, 203)
+    doc.text('A/c No : 00000000', 6, 209)
+    doc.text('Branch & IFS CODE : 357657', 6, 215)
     doc.setFontSize(8);
-    doc.setFont('helvetica', 'bold');
-    doc.text(`Sub Total : ${this.invoiceDetails.subTotal}`, 155, 196)
-    doc.text(`Discount: ${this.invoiceDetails.discount}`, 155, 202)
-    doc.text(`Transport Charges: ${this.invoiceDetails.transportCharges}`, 155, 208)
-    doc.text(`Loading Charges : ${this.invoiceDetails.loadingCharges}`, 155, 214)
-    doc.text(`UnLoading Charges : ${this.invoiceDetails.unloadingCharges}`, 155, 220)
-    doc.text(`Total Amount : ${this.invoiceDetails.totalAmount}`, 155, 226)
-    doc.text(`Paid Amount (19/07/2025) : ${this.invoiceDetails.paidAmount}`, 155, 232)
-    doc.text(`Remaining Amount : ${this.invoiceDetails.remainingAmount}`, 155, 238
-    )
-    doc.setFont('helvetica', 'bold');
+    // === Main Summary Box ===
+    doc.setFillColor(255, 255, 255); // Light background
+    doc.setDrawColor(0, 0, 0);       // Border color
+    doc.roundedRect(139, 194, 65, 68, 0, 0, 'FD'); // (x, y, w, h, rx, ry, Fill+Draw)
+
+    // === Text inside box ===
+    doc.setTextColor(0);
+    doc.setFontSize(12);
+
+    doc.text("Sub Total", 142, 202);
+    doc.text(`${this.invoiceDetails.subTotal}`, 192, 202);
+
+    doc.text("Discount", 142, 210);
+    doc.text(`${this.invoiceDetails.discount}`, 199, 210);
+
+    doc.text("Transport Charges", 142, 218);
+    doc.text(`${this.invoiceDetails.transportCharges}`, 199, 218);
+
+    doc.text("Loading Charges", 142, 226);
+    doc.text(`${this.invoiceDetails.loadingCharges}`, 199, 226);
+
+    doc.text("UnLoading Charges", 142, 234);
+    doc.text(`${this.invoiceDetails.unloadingCharges}`, 199, 234);
+
+    // === Total Amount Bar ===
+    doc.setFillColor(53, 53, 53); // Dark gray background
+    doc.roundedRect(141, 237, 61, 8, 0, 0, 'F'); // Rounded bar
+    doc.setTextColor(255, 255, 255);
+    doc.text("Total Amount", 142, 242);
+    doc.text(`${this.invoiceDetails.totalAmount}`, 192, 242);
+
+    // === Remaining Info ===
+    doc.setTextColor(0, 0, 0);
+    doc.text("Paid Amount", 142, 250);
+    doc.setFontSize(8);
+    doc.text("(20/06/2025)", 167, 249.5);
     doc.setFontSize(10);
-    doc.text('For National Entreprises', 158, 254)
-    doc.line(155, 264, 195, 264);
-    doc.setFontSize(8);
+    doc.text(`${this.invoiceDetails.paidAmount}`, 193.5, 250);
+
+    doc.text("Remaining Amount", 142, 256);
+    doc.text(`${this.invoiceDetails.remainingAmount}`, 199, 256);
+
     doc.setFont('helvetica', 'bold');
-    doc.text("Authorised signatory", 162, 268)
+    doc.setFontSize(12);
+    doc.text('For National Enterprises', 6, 240)
+    doc.line(6, 264, 43, 264);
+    doc.setFontSize(10);
+    doc.setFont('helvetica', 'bold');
+    doc.text("Authorised signatory", 6, 268)
     doc.rect(4, 276, 202, 12);
     doc.text("Declaration", 8, 280)
     doc.setFont('helvetica', 'normal');
